d8a6d12a7feb7432239e95095ad45b6f983419a2
jociane <jocianefranzoni@gmail.com>
2024-04-10 10:55:26 -0300

    Set browser drive path and add the driver folder to gitignore

--- Introduction/src/SeIntroduction.java
+++ Introduction/src/SeIntroduction.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeIntroduction {
@@ -5,7 +6,9 @@ public class SeIntroduction {
 	public static void main(String[] args) {
 		// Invoking Browser
 		//Chrome - ChromeDriver -> Methods
-		ChromeDriver driver = new ChromeDriver();
+		//Crete drivers/ and add the browser drivers (exe) to be used
+		System.getProperty("webdriver.chrome.driver", "src\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
 	}
 
