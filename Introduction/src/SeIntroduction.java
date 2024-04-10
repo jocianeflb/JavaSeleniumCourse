769fde6671636e0c37eca6899c161903b38bc529
jociane <jocianefranzoni@gmail.com>
2024-04-10 09:52:50 -0300

    Convert to maven project, add dependencies and create Java class with Chome Web Driver

---
+++ Introduction/src/SeIntroduction.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeIntroduction {
+
+	public static void main(String[] args) {
+		// Invoking Browser
+		//Chrome - ChromeDriver -> Methods
+		ChromeDriver driver = new ChromeDriver();
+
+	}
+
+}
